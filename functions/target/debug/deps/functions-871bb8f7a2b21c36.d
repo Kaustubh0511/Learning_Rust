d466a0133da26f515f777334ec53b0292641d0f7
Kaustubh0511 <kaustubhdayma@gmail.com>
2024-05-06 10:45:40 +0530

    Rust changes

---
+++ functions/target/debug/deps/functions-871bb8f7a2b21c36.d
@@ -0,0 +1,6 @@
+/home/kpit/Desktop/Rust/functions/target/debug/deps/libfunctions-871bb8f7a2b21c36.rmeta: src/main.rs src/test.rs
+
+/home/kpit/Desktop/Rust/functions/target/debug/deps/functions-871bb8f7a2b21c36.d: src/main.rs src/test.rs
+
+src/main.rs:
+src/test.rs:
